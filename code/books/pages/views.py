edefc3ea898b84ea2f0ed5f1a590a7f705f973b6
alex-njuguna <njugunakinuthia013@gmail.com>
2024-05-09 02:29:30 +0300

    sets up about page

--- code/books/pages/views.py
+++ code/books/pages/views.py
@@ -4,3 +4,6 @@ from django.views.generic import TemplateView
 
 def home(request):
     return render(request, 'home.html', {'title': 'Home'})
+
+def about(request):
+    return render(request, 'about.html', {'title': 'About'})
