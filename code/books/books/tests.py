e80309ccb05720e19372e3af9279d012767b28cd
alex-njuguna <njugunakinuthia013@gmail.com>
2024-05-11 21:23:46 +0300

    tests to create a book, display it in list and display the book specs

--- code/books/books/tests.py
+++ code/books/books/tests.py
@@ -1,3 +1,33 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# Create your tests here.
+from .models import Book
+
+
+class BookTests(TestCase):
+
+    def setUp(self):
+        self.book = Book.objects.create(
+            title = 'After 4:30',
+            author = 'Ben Maillu',
+            price='10.00'
+        )
+
+    def test_book_listing(self):
+        self.assertEqual(self.book.title, 'After 4:30')
+        self.assertEqual(self.book.author, 'Ben Maillu')
+        self.assertEqual(self.book.price, '10.00')
+
+    def test_book_list_view(self):
+        response = self.client.get(reverse('books_list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'After 4:30')
+        self.assertTemplateUsed(response, 'books/books_list.html')
+
+    def test_book_detail_view(self):
+        response = self.client.get(self.book.get_absolute_url())
+        no_response = self.client.get('/books/12345')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(no_response.status_code, 404)
+        self.assertContains(response, 'Ben Maillu')
+        self.assertTemplateUsed(response, 'books/book_detail.html')
