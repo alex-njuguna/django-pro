72e7e0dbbb0e31d5edfa3142ad763a2cfbb95863
alex-njuguna <njugunakinuthia013@gmail.com>
2024-05-15 02:24:21 +0300

    post detail view

--- code/a_blog/mysite/blog/views.py
+++ code/a_blog/mysite/blog/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+from django.http import Http404
 
 from .models import Post
 
 def post_list(request):
     posts = Post.published.all()
     return render(request, 'blog/post_list.xhtml', {'posts':posts})
+
+def post_detail(request, id):
+    try:
+        post = Post.published.get(id=id)
+    except:
+        raise Http404('No post found.')
+    return render(request, 'blog/post_detail.xhtml', {'post':post})
