6c64d9030a4dcd9b04a0415fe67822df2076cc0a
alex-njuguna <njugunakinuthia013@gmail.com>
2024-05-16 04:16:45 +0300

    post_comment functionality

--- code/a_blog/mysite/blog/views.py
+++ code/a_blog/mysite/blog/views.py
@@ -3,9 +3,10 @@ from dotenv import load_dotenv
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 from django.core.mail import send_mail
+from django.views.decorators.http import require_POST
 
-from .models import Post
-from .forms import EmailPostForm
+from .models import Post, Comment
+from .forms import EmailPostForm, CommentForm
 
 load_dotenv()
 
@@ -44,3 +45,16 @@ def post_share(request, post_id):
     
     return render(request, 'blog/post_share.xhtml', {'form': form, 'post': post, 'sent': sent, 'title': 'share post'})
 
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    comment = None
+    # a comment was posted
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        # assign the post to the comment
+        comment.post = post
+        comment.save()
+
+    return render(request, 'blog/comment.xhtml', {'post': post, 'form': form, 'comment': comment})
