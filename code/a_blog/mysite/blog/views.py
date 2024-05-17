5edbeeb56d3f843693c2268a5719c44780831fb3
alex-njuguna <njugunakinuthia013@gmail.com>
2024-05-17 07:53:07 +0300

    stemming and ranking results

--- code/a_blog/mysite/blog/views.py
+++ code/a_blog/mysite/blog/views.py
@@ -6,7 +6,7 @@ from django.core.mail import send_mail
 from taggit.models import Tag
 from django.core.paginator import Paginator
 from django.db.models import Count
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 
 from .models import Post, Comment
 from .forms import EmailPostForm, CommentForm, SearchForm
@@ -88,5 +88,7 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
-            results = Post.published.annotate(search=SearchVector('title', 'body'),).filter(search=query)
+            search_vector = SearchVector('title', 'body')
+            search_query = SearchQuery(query)
+            results = Post.published.annotate(search=search_vector, rank=SearchRank(search_vector, search_query),).filter(search=search_query).order_by('-rank')
     return render(request, 'blog/search.xhtml', {'form': form, 'query': query, 'results': results})
