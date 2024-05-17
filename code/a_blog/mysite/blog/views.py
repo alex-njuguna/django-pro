b022d03ecdf1565ebf716b04640f433cc6b6af1a
alex-njuguna <njugunakinuthia013@gmail.com>
2024-05-17 07:29:20 +0300

    basic search engine functionality

--- code/a_blog/mysite/blog/views.py
+++ code/a_blog/mysite/blog/views.py
@@ -6,9 +6,10 @@ from django.core.mail import send_mail
 from taggit.models import Tag
 from django.core.paginator import Paginator
 from django.db.models import Count
+from django.contrib.postgres.search import SearchVector
 
 from .models import Post, Comment
-from .forms import EmailPostForm, CommentForm
+from .forms import EmailPostForm, CommentForm, SearchForm
 
 load_dotenv()
 
@@ -78,4 +79,14 @@ def post_share(request, post_id):
         form = EmailPostForm()
     
     return render(request, 'blog/post_share.xhtml', {'form': form, 'post': post, 'sent': sent, 'title': 'share post'})
-    
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            results = Post.published.annotate(search=SearchVector('title', 'body'),).filter(search=query)
+    return render(request, 'blog/search.xhtml', {'form': form, 'query': query, 'results': results})
