0e06117e6495206fd156f22b0f83f168cc977503
alex-njuguna <njugunakinuthia013@gmail.com>
2024-05-17 13:21:03 +0300

    login functionality and template

---
+++ code/a_blog/mysite/users/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+
+app_name = 'users'
+
+urlpatterns = [
+    path('login/', LoginView.as_view(template_name='users/login.xhtml'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='users/logout.xhtml'), name='logout'),
+]
