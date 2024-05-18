03719f304f8e8b354297a178a4d92c6235159d3c
alex-njuguna <njugunakinuthia013@gmail.com>
2024-05-18 22:42:35 +0300

    improving comments and create post forms with widgets

--- code/a_blog/mysite/blog/forms.py
+++ code/a_blog/mysite/blog/forms.py
@@ -11,16 +11,40 @@ class EmailPostForm(forms.Form):
     comments = forms.CharField(required=False, widget=forms.Textarea)
 
 class CommentForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['name'].label = ''  
+        self.fields['email'].label = ''  
+        self.fields['body'].label = ''  
+
     class Meta:
         model = Comment
         fields = ['name', 'email', 'body']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control my-2', 'placeholder': 'Name'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control my-2', 'placeholder': 'Email'}),
+            'body': forms.Textarea(attrs={'class': 'form-control my-2', 'placeholder': 'Comment'}),
+        }
+
 
 class SearchForm(forms.Form):
     query = forms.CharField(label='')
 
 class NewPostForm(forms.ModelForm):
-    tags = TagField()
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['title'].label = ''
+        self.fields['body'].label = ''
+        self.fields['tags'].label = ''
+        self.fields['status'].label = ''
 
     class Meta:
         model = Post
         fields = ['title', 'body', 'tags', 'status']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control my-2', 'placeholder': 'Title'}),
+            'body': forms.Textarea(attrs={'class': 'form-control my-2', 'placeholder': 'Body'}),
+            'tags': forms.TextInput(attrs={'class': 'form-control my-2', 'placeholder': 'Tags'}),
+            'status': forms.Select(attrs={'class': 'form-select my-2'}),
+        }
+
--- code/a_blog/mysite/mysite/settings.py
+++ code/a_blog/mysite/mysite/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     # extensions
     'crispy_forms',
     'taggit',
+    'ckeditor',
 
     # local
     'blog.apps.BlogConfig',
