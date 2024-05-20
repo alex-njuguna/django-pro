da1d20fb9229e19fba51aa83341f0e155f9d9ec5
alex-njuguna <njugunakinuthia013@gmail.com>
2024-05-20 12:55:47 +0300

    password reset via email

--- code/social/account/urls.py
+++ code/social/account/urls.py
@@ -7,10 +7,17 @@ urlpatterns = [
     path('login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
 
+    # change password
     path('password-change/', auth_views.PasswordChangeView.as_view(template_name='registration/password_changeform.xhtml'), name='password_change'),
-
     path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='registration/password_change_done.xhtml'), name='password_change_done'),
-    
+
+    # reset password
+    path('password-reset/', auth_views.PasswordResetView.as_view(template_name='registration/password_reset_form.xhtml'), name='password_reset'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_done.xhtml'), name='password_reset_done'),
+    path('password-reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.xhtml'), name='password_reset_confirm'),
+    path('password-reset/complete', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.xhtml'), name='password_reset_complete'),
+
+    # dashboard
     path('', views.dashboard, name='dashboard'),
 ]
 
