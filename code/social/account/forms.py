71c29c3757a36283107d2de86695ec0005e221a6
alex-njuguna <njugunakinuthia013@gmail.com>
2024-05-21 08:20:03 +0300

    improvements to register user form

--- code/social/account/forms.py
+++ code/social/account/forms.py
@@ -1,5 +1,33 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 class LoginForm(forms.Form):
     username = forms.CharField(label='username')
     password = forms.CharField(widget=forms.PasswordInput)
+
+class UserRegisterForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'email', 'password1', 'password2']
+
+        widgets = {
+            'username': forms.TextInput(attrs= {
+                'class': 'form-control, my-3',
+                'placeholder': 'e.g. peter_k'
+            }),
+            'first_name': forms.TextInput(attrs= {
+                'class': 'form-control, my-3',
+                'placeholder': 'peter'
+            }),
+            'email': forms.TextInput(attrs= {
+                'class': 'form-control, my-3',
+                'placeholder': 'peter.k@example.com'
+            }),
+            'password1': forms.PasswordInput(attrs= {
+                'class': 'form-control, my-3'
+            }),
+            'password2': forms.PasswordInput(attrs= {
+                'class': 'form-control, my-3'
+            }),
+        }
