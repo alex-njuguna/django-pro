8b202336c66b755bdaf9b5db5c7ce0deb2fa4f91
alex-njuguna <njugunakinuthia013@gmail.com>
2024-05-22 19:35:21 +0300

    othe minor styling modifications

--- code/social/account/forms.py
+++ code/social/account/forms.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 from .models import Profile
 
 class LoginForm(forms.Form):
-    username = forms.CharField(label='username')
+    username = forms.CharField(label='username/email')
     password = forms.CharField(widget=forms.PasswordInput)
 
 class UserRegisterForm(UserCreationForm):
