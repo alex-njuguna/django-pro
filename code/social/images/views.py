13b4c3b41a32b74e34e75f1833f3642e4d77a2a8
alex-njuguna <njugunakinuthia013@gmail.com>
2024-05-23 10:22:27 +0300

    image creation view

--- code/social/images/views.py
+++ code/social/images/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contib import messages
 
-# Create your views here.
+from .forms import ImageCreateForm
+from .models import Image
+
+
+@login_required
+def image_create(request):
+    if request.method == 'POST':
+        form = ImageCreateForm()
+        if form.is_valid():
+            cd = form.cleaned_data
+            new_image = form.save(commit=False)
+            new_image.user = request.user
+            new_image.save()
+            messages.success(request, 'Image added successfully')
+            return redirect(new_image.get_absolute_url)
+    else:
+        form =ImageCreateForm()
+    context = {
+        'form': form,
+        'title': 'upload image',
+    }
+
+    return render(request, 'images/image/create.xhtml')
+
+
